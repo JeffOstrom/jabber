cafb4f061c6b320e68e6e9802b9b4dc39e00d8c3
Peter Peck <karatekid4christ@gmail.com>
2018-01-05 21:23:24 -0600

    Adding passport code for authentication in registration.js file

--- routes/registration.js
+++ routes/registration.js
@@ -4,13 +4,25 @@ var bcrypt 			= require('bcryptjs');
 var passport 		= require('passport'), 
 var LocalStrategy 	= require('passport-local').Strategy;
 var db				= require("../models")
-var User 			= require("..models/accounts")
-//Register User
-router.get('/signup'/*/registration*/, function(req, res, next) {
 
+var User 			= require("..models/accounts")
+
+passport.serializeUser(function(user, done) {
+  	done(null, user._id);
 });
 
-router.post('/signup'/*/registration*/, function(req, res, next) {
+passport.deserializeUser(function(id, done) {
+ 	User.findById(id, function(err, user) {
+    	done(err, user);
+  	});
+});
+
+//Register User
+router.get('/signup', function(req, res, next) {
+	res.render('signup');
+});
+
+router.post('/signup', function(req, res, next) {
 	
 	var firstName 	= req.body.username;
 	var	lastname    = req.body.lastname;
@@ -24,34 +36,52 @@ router.post('/signup'/*/registration*/, function(req, res, next) {
 	req.checkBody('lastname',	'Name is required').notEmpty();
 	req.checkBody('email', 		'Email is required').isEmail();
 	req.checkBody('password', 	'Password is required').notEmpty();
-	req.checkBody('password2', 	'Password does not match').equals(req.body.password);
+	req.checkBody('cpassword', 	'Password does not match').equals(req.body.password);
+	req.checkBody('bio', 		'Please tell us something about yourself').notEmpty();
+	
 
 	var errors		= req.validationErrors()
 	if (errors) {
 		res.render('signup')
-	} else {
-		var newUser = new user({
-			//New user information goes here and gets render 
-		})
-	};
+	} 
 });
 
-passport.use(new LocalStrategy (
-	function(username, password, done) {
+passport.use('local.signup', new LocalStrategy ({
+	usernameField: 'email',
+	usernameField: 'email',
+	passReqToCallback: true
+},	function(req, username, password, done) {
 		User.findOne({ username: username }, function(err, user) {
 			if (err) { 
 				return done(err); 
 			}
-			if (!user) {
+
+			if(user) {
+				return done(null, user);
+			} else {
 				return done(null, false, { message: 'Incorrect username.' });
 			}
 
 			if (!user.validPassword(password)) {
 				return done(null, false, { message: 'Incorrect password.' });
 			}
-			return done(null, user);
+
+			/*Creates new instance*/
+			var newUser 		= new User();
+			newUser.firstname	= req.body.firstname;
+			newUser.lastname	= req.body.lastname;
+			newUser.email		= req.body.email;
+			newUser.password 	= req.body.password;
+
+			newUser.save(function(err){
+				if(err) {
+					return done(err);
+				} 
+
+				return done(null, newUser);
+			});
 		});
-	}
+	};
 ));
 
 module.exports		= router;
