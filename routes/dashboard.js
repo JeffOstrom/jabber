9ae03b704c69d9cb2bcb1c63f42026c24e51a367
fjnw <faizanjamal2018@u.northwestern.edu>
2018-01-10 02:32:35 -0600

    Public folder organization. Added POST in dashboard.js. Added JS links in handlebar files.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,7 @@
 var path = require("path");
 var express = require('express');
 var router = express.Router();
-
-router.get('/dashboard', ensureAuthenticated, function(req, res, next) {
-	res.render('dashboard');
-});
+var models = require("../models");
 
 function ensureAuthenticated(req, res, next){
     if(req.isAuthenticated()){
@@ -13,4 +10,31 @@ function ensureAuthenticated(req, res, next){
         res.render('index');
     }
 }
+
+router.get('/dashboard', ensureAuthenticated, function(req, res) {
+    res.render('dashboard');
+});
+
+/*Post Message*/
+router.post('/dashboard', function(req, res) {
+
+	console.log("locals F: " + res.locals.user.firstname);
+
+
+	/*Creating new message*/
+	var newMessage = {
+		user: res.locals.user.firstname + " " + res.locals.user.lastname,
+		message: req.body.message
+	};
+
+	/*Post new message*/
+	models.Messages.create(newMessage).then(function(message){
+		req.flash('success_msg', 'Message successfully sent');
+		res.redirect('/dashboard');
+	});
+
+});
+
+
+
 module.exports = router;
